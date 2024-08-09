3a2683cd358c2a86739514402a02a6d68ffe0258
DanZmanGrant <dan@zygadox.com>
2024-08-09 12:21:24 +1000

    Add resume metadata

--- src/app/(blogs)/crafting-the-perfect-teacher-resume/page.tsx
+++ src/app/(blogs)/crafting-the-perfect-teacher-resume/page.tsx
@@ -5,10 +5,11 @@ import type { Metadata } from 'next';
 
 export const metadata: Metadata = {
   title: {
-    absolute: 'Expert Resume Tips for Educators',
+    absolute:
+      'Crafting the Perfect Teacher Resume: Expert Tips for Landing Your Dream Teaching Job',
   },
-  description: ``,
-  keywords: ``,
+  description: `Discover essential tips for creating an effective teacher resume. Learn how to highlight your skills, quantify achievements, and avoid common pitfalls to stand out in the competitive job market and secure your ideal teaching position.`,
+  keywords: `teacher resume tips, crafting teacher resume, teacher CV advice, teaching job application, educational resume writing, standout teacher resume, teacher job search, educator resume guide, resume tips for teachers, teacher resume examples`,
 };
 
 const tableOfContents = [
